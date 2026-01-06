3c09ae5b4548c27ae5d620d20c5c94ca088b3565
jtrecenti <julio.trecenti@gmail.com>
2026-01-06 15:29:11 -0300

    remove unused

--- data-raw/8-model.R
+++ data-raw/8-model.R
@@ -46,36 +46,3 @@ tab02 <- c(models_fips_nounk_inter, models_state_nounk_inter) |>
   fixest::etable()
 
 print(tab02)
-
-
-da_model_state |>
-  dplyr::filter(
-    year >= min(years_available()[["fe"]]),
-    year <= max(years_available()[["fe"]]),
-    shr > 0 | fe > 0,
-    race != "Unknown/Others",
-    sex != "Unknown/Others"
-  ) |>
-  tidyr::drop_na()
-
-
-yr_offset <- years_available()[["mpv"]]
-yr_shr <- years_available()[["shr"]]
-da_model_state |>
-  dplyr::filter(
-    year >= max(yr_offset[1], yr_shr[1]),
-    year <= min(yr_offset[2], yr_shr[2])
-  ) |>
-  dplyr::mutate(
-    ofs = .data[["mpv"]],
-    ofs = dplyr::na_if(ofs, 0),
-    ofs = ifelse(is.na(ofs), 0 + .5 * (shr > 0), ofs)
-  ) |>
-  dplyr::filter(
-    shr > 0 | ofs > 0,
-    race != "Unknown/Others",
-    sex != "Unknown/Others"
-  ) |>
-  tidyr::drop_na()
-
-## log \theta
